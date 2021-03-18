715da416058fa14a9d96ad593ef3b8a8af7bcd0d
aishahrosli <aishahrosli61@gmail.com>
2021-03-18 22:10:24 +0800

    git us cool

--- samples/src/samples/demo/GitTest.java
+++ samples/src/samples/demo/GitTest.java
@@ -1,9 +1,9 @@
-package samples.demo;
+	package samples.demo;
 
 public class GitTest {
 
 	public static void main (String[]args) {
 	
-		System.out.println ("Git is fun");
+		System.out.println ("Git is cool");
 	}
 }
