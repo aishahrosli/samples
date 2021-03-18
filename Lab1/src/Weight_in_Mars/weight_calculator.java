2a79b901cb829ab4fa5f38b01c11d4edc62cfc01
aishahrosli <aishahrosli61@gmail.com>
2021-03-18 23:32:15 +0800

    lab1

---
+++ Lab1/src/Weight_in_Mars/weight_calculator.java
@@ -0,0 +1,29 @@
+package Weight_in_Mars;
+
+import java.util.Scanner;
+
+public class weight_calculator {
+	
+	  
+
+    public static void main(String[] args) throws Exception {
+        calculateWeight();
+    }
+ 
+    private static void calculateWeight() throws Exception {
+        System.out.print("Please enter your weight in kg: ");
+        Scanner s = new Scanner(System.in);
+        double weight = s.nextFloat();
+        
+         
+        // multiplication by 100*100 for cm to m conversion
+        double bmi = (weight/9.81)*3.711;
+         
+        System.out.println("Your BMI is: "+bmi);
+      
+         
+    }
+}
+ 
+ 
+
